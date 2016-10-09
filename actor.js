7b8c70e3bd6bb5f7612c9fdbc576001c31289fe4
Zoli <zoltan@kosina.hu>
2016-10-09 11:01:26 +0200

    Chase

--- actor.js
+++ actor.js
@@ -4,15 +4,41 @@ RL.Actor = function (x, y, scheduler) {
   'use strict';
   this.x = x;
   this.y = y;
+  this.target = {
+    x: x,
+    y: y
+  };
   this.scheduler = scheduler;
   this.scheduler.add(this, true);
 };
 
 RL.Actor.prototype.act = function () {
   'use strict';
-  var x, y;
-  x = Math.floor(Math.random() * 3 - 1);
-  y = Math.floor(Math.random() * 3 - 1);
-  RL.move(this, x, y);
+  RL.fov.compute(this.x, this.y, 80, this.addTarget.bind(this));
+  this.moveTowardsTarget();
   this.scheduler.setDuration(2);
+};
+
+RL.Actor.prototype.addTarget = function (x, y, r, visibility) {
+  'use strict';
+  if (x === RL.player.x && y === RL.player.y) {
+    this.target = {
+      x: x,
+      y: y
+    };
+  }
+};
+
+RL.Actor.prototype.moveTowardsTarget = function () {
+  'use strict';
+  if (this.target.x < this.x) {
+    RL.move(this, -1, 0);
+  } else if (this.target.x > this.x) {
+    RL.move(this, 1, 0);
+  }
+  if (this.target.y > this.y) {
+    RL.move(this, 0, 1);
+  } else if (this.target.y < this.y) {
+    RL.move(this, 0, -1);
+  }
 };
